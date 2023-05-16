105d46f997b639cfb70968ce96bbaf9695cd191b
zhao- <calcite@139.com>
2023-05-16 12:17:12 +0800

    init

---
+++ app/src/main/java/pers/u8f23/template_android_project/MainActivity.java
@@ -0,0 +1,14 @@
+package pers.u8f23.template_android_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
